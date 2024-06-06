ca4c1263733dfaf31dd83d897700881e2505b786
cchiffot <calel44jke@gmail.com>
2024-06-06 17:18:09 +0200

    update: creategame ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import { BrowserRouter as  Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/Home";
 import CreateGame from "./components/CreateGame";
 import JoinGame from "./components/JoinGame";
@@ -14,18 +14,20 @@ function App() {
 						The Bad Reviews
 					</Link>
 				</header>
+				<div className="flex flex-col items-center justify-center min-h-screen">
 					<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/game/new" element={<CreateGame />} />
-							<Route path="/game/join" element={<JoinGame />} />
-							<Route path="*" element={<NotFound />} />
+						<Route path="/" element={<Home />} />
+						<Route path="/game/new" element={<CreateGame />} />
+						<Route path="/game/join" element={<JoinGame />} />
+						<Route path="*" element={<NotFound />} />
 					</Routes>
+				</div>
 			</div>
 		</Router>
 	);
 }
 
-function NotFound(){
+function NotFound() {
 	return (
 		<div>
 			<h1>404 - Not Found</h1>
--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -4,15 +4,34 @@ import React, { useState } from "react";
 export default function CreateGame() {
 	const [username, setUsername] = useState("")
 
-	function handleCreateGame(){
-		console.log('create')
+	function handleCreateGame() {
+		console.log('create game, username :', username)
 	}
 
+	const isDisabled = !username;
+
 	return (
-		<div>
+		<div >
 			<h1 className="font-bold">CreateGame</h1>
-			<input type="text" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-			<button onClick={handleCreateGame}>Create Game</button>
+			<div>
+				<input
+					className="px-4 py-2 mb-4 border rounded mr-4"
+					required type="text"
+					placeholder="Enter your username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<button
+					className={`px-4 py-2 font-bold rounded ${isDisabled
+						? 'bg-gray-400 text-gray-700 cursor-not-allowed'
+						: 'bg-blue-500 text-white hover:bg-blue-700'
+						}`}
+					onClick={handleCreateGame}
+					disabled={!username}
+				>
+					Create Game
+				</button>
+			</div>
 		</div>
 	)
 
