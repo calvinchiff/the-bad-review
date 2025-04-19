a1ffcff072bd855806580d7b19d647ea0b9ea43e
calvinchiff <calel44jke@gmail.com>
2025-04-19 18:02:38 +0200

    init: restarted the project using next

--- src/components/PlayerCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./PlayerCard.css"
-import PlayerImage from './PlayerImage'
-
-export default function PlayerCard() {
-    return (
-        <PlayerImage />
-    )
-}
