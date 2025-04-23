9040a7d8f103926ffdaf9d2beddb073d275c446f
calvinchiff <calel44jke@gmail.com>
2025-04-23 12:29:28 +0200

    feat: portfolio & github links in footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
 	return (
-		<footer className="h-50 flex w-full font-jersey-25 items-center justify-center text-white/60">
+		<footer className="h-50 w-full font-jersey-25 flex items-center justify-center gap-4 text-white/60">
 			<p className="text-base">
-				© {new Date().getFullYear()} CalvinChiff. All rights reserved.
+				© {new Date().getFullYear()}{" "}
+				<a
+					href="https://calvinchiff.com"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="underline hover:text-white transition"
+				>
+					CalvinChiff
+				</a>
+				. All rights reserved.
 			</p>
+
+			<a
+				href="https://github.com/calvinchiff"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text-xl hover:text-white transition"
+			>
+				<FaGithub />
+			</a>
 		</footer>
 	);
 }
