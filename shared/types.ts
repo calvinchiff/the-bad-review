489903ec6c5a80814944693bca4992358e5b3c17
calvinchiff <calel44jke@gmail.com>
2025-05-28 10:56:57 +0200

    feat: Shared types

---
+++ shared/types.ts
@@ -0,0 +1,26 @@
+export type Player  {
+    id: string;
+    username: string;
+    letterboxd: string;
+    avatar: string;
+    score: number;
+    reviews: string[];
+}
+
+export type Question = {
+    id: number;
+    text: string;
+    review: string;
+    options: string[];
+    correctAnswer: string;
+}
+
+export type GameState = {
+    roomId: string;
+    players: Player[];
+    questions: Question[];
+    roundDuration: number;
+    currentTimer: NodeJS.Timeout | null;
+    currentQuestionIndex: number;
+    started: boolean;
+}
